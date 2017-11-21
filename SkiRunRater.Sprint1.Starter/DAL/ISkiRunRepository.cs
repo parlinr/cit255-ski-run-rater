dc3cf5b83c17e890900301ca33434f72d6babdbc
Ryan Parlin <parlinr@ad.nmc.edu>
2017-11-20 19:40:55 -0500

    Sprint 4 complete

---
+++ SkiRunRater.Sprint1.Starter/DAL/ISkiRunRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiRunRater
+{
+    public interface ISkiRunRepository
+    {
+        List<SkiRun> SelectAll();
+        SkiRun SelectById(int id);
+        void Insert(SkiRun obj);
+        void Update(SkiRun obj);
+        void Delete(int id);
+        void Save();
+
+    }
+}
